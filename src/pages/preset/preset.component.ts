c82cf7ea2b54986ca6aa26b74dd90bb66cc26870
Steve Zheng <steve.zheng@globallogic.com>
2017-03-26 18:34:02 -0400

    added search bar

--- src/pages/preset/preset.component.ts
+++ src/pages/preset/preset.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { PlanService } from '../../app/services/plan.service';
+import { IHiitPlan } from '../../app/entities/hiit-plan.interface';
 
 @Component({
   selector: 'page-preset',
   templateUrl: 'preset.component.html'
 })
-export class PresetPage {
+export class PresetPage implements OnInit {
+  editMode: boolean;
+  searchString: string;
 
   constructor(public navCtrl: NavController, private planService: PlanService) {
     this.planService.createPlan({name: 'Default Plan', sets: 5, restTime: 90, actionTime: 30, actions: 2})
@@ -15,4 +18,24 @@ export class PresetPage {
     console.log(this.planService.plans)
   }
 
+  ngOnInit(): void {
+    this.editMode = false;
+    this.searchString = '';
+  }
+
+  togglePlanEditor() {
+    this.editMode = !this.editMode;
+    if (this.editMode) {
+      // just give a empty object
+      this.planService.tempPlan = {} as IHiitPlan;
+    }
+  }
+
+  onSearchInput(event): void {
+
+  }
+
+  onSearchCancel(event): void {
+
+  }
 }
