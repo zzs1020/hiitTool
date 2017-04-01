ba41a976591c4b15db109480eec8e904ced55ebe
Steve Zheng <steve.zheng@globallogic.com>
2017-04-01 15:11:28 -0400

    solve keyboard jump issue

--- src/app/shared-cmps/input-group/input-group.component.ts
+++ src/app/shared-cmps/input-group/input-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { PlanService } from '../../services/plan.service';
+import { Keyboard } from '@ionic-native/keyboard';
 
 @Component({
   selector: 'app-input-group',
@@ -10,9 +11,9 @@ export class InputGroupComponent {
   @Input() presetMode?: boolean;
   @Output() savedStatus: EventEmitter<boolean>;
 
-  //todo: bug: app shakes when input
-  constructor(public planService: PlanService) {
+  constructor(public planService: PlanService, private keyboard: Keyboard) {
     this.savedStatus = new EventEmitter();
+    keyboard.disableScroll(true);
   }
 
   savePlan(): void {
